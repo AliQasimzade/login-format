599493df439e96df21f9e6985d47e4df3de65268
Ali Qasimzade <ali@globalsoft.az>
2022-09-22 14:14:13 +0400

    fi

--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ import App from "./App";
 import { RecoilRoot } from "recoil";
 import { BrowserRouter } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
+import * as serviceWorker from './serviceWorker';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
+root.render( 
     <RecoilRoot>
       <HelmetProvider>
         <BrowserRouter>
@@ -15,5 +16,12 @@ root.render(
         </BrowserRouter>
       </HelmetProvider>
     </RecoilRoot>
-  </React.StrictMode>
 );
+
+// If you want to enable client cache, register instead.
+serviceWorker.unregister();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
